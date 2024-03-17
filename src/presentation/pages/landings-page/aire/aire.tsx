6040b265ff5d99d39486ab57fe5a337e531f59a9
Kátia Cibele <katiacih@gmail.com>
2024-03-17 23:40:58 +0000

    feat: cria esqueleto de pagina aire

---
+++ src/presentation/pages/landings-page/aire/aire.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import LandingContent from '../../home/landing-content';
+
+function AirePage() {
+  const title = `Aire`;
+  return (
+    <LandingContent title={title}>
+      <div>AIRE</div>
+      <div>circles with colors</div>
+      <div>tow columns</div>
+      <div>typo</div>
+      <div>montagem de imagens</div>
+    </LandingContent>
+  )
+}
+
+export default AirePage;
